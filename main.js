f0d148cd10fe305175532f5f213343e2bafea08c
Dorrito5653 <dorritoshah1543@gmail.com>
2024-05-18 12:11:46 -0400

    edited server

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
 const app = express()
 const ai_router = require('./routes/ai_router')
 const port = 3000;
@@ -9,6 +10,19 @@ app.use(express.json())
 app.use(cors)
 app.use(ai_router)
 
-app.listen(`http://localhost:${port}/pantryapp`, () => {
+app.listen(port, () => {
     console.log(`App listening on port ${port}!`);
 })
+
+async function dbConnect() {
+    mongoose.connect(process.env.MONGODB_KEY).then(() => {
+        console.log("Database successfully connected")
+    }).catch((error) => {
+        console.error(error)
+    })
+    connection = mongoose.connection
+    return connection
+}
+
+var mongoose_connection = dbConnect()
+console.log(mongoose_connection)
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const mongodb = require("mongoose");
+const mongoose = require("mongoose");
 
-const userAccount = new mongodb.Schema({
+const UserSchema = new mongoose.Schema({
     email: String,
     password: String,
+    token: String,
     items: [{
         name: String,
         expiry: Date,
@@ -11,3 +12,45 @@ const userAccount = new mongodb.Schema({
     }]
 })
 
+const User = mongoose.model('users', UserSchema)
+
+function rand() {
+    return Math.random().toString(36).substr(2); // remove `0.`
+};
+
+function token() {
+    return rand() + "-" + rand(); // to make it longer
+};
+
+const router = express.Router()
+
+router.post('/users', async (req, res) => {
+    try {
+        const new_user = new User({
+            email: req.body.email,
+            password: req.body.password,
+            token: token(),
+            items: req.body.items
+        })
+        new_user.save()
+        res.status(200).send(new_user)
+    } catch (error) {
+        console.error(error)
+        res.status(500).send('There was an error creating the account')
+    }
+    
+})
+
+router.get('/users/:token', async (req, res) => {
+    try {
+        const acc = await User.find({token: req.params.token})
+        if (acc == null) {
+            res.status(404).send('This account does not exist')
+        } else {
+            res.status(200).send(acc)
+        }
+    } catch (error) {
+        console.error(error)
+        res.status(500).send('There was an error getting the account')
+    }
+})
