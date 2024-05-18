5392219f9c2634486591d94b02ba33a1785e1411
username121546434 <gokuleatharv06@gmail.com>
2024-05-18 00:21:52 -0400

    Added addFood function in script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function addFood() {
-    
+    const quantity_element = document.getElementById('enter_quantity')
+    quantity = Number(quantity_element.value.trim())
+    const food_element = document.getElementById('enter_food')
+    food = food_element.value.trim()
+
+    if (item === '') {
+        
+    }
 }
