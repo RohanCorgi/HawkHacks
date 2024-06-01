6251b95ca4dac8937d0302c17e150b3aefcf1200
Ishaan Dhiman <ishaandhiman74@gmail.com>
2024-06-01 18:47:20 -0400

    Improved readability of the code

--- app/script.js
+++ app/script.js
@@ -1,12 +1,13 @@
-var foods_info = []
-var color = 'black'
-let text_colors = {
+let foodsInfo = [];
+let color = 'black';
+
+const textColors = {
     red: "rgb(241, 47, 47)",
     green: "rgb(77, 250, 77)",
     blue: "rgb(74, 74, 243)",
     white: "white",
     black: "rgb(0, 0, 0)"
-}
+};
 
 function generateAlphabeticalID(length) {
     let id = '';
@@ -16,7 +17,7 @@ function generateAlphabeticalID(length) {
     return id.substring(0, length);
 }
 
-function addItem(is_shopping_list) {
+function addItem(isShoppingList) {
     const quantityElement = document.getElementById('enter_quantity');
     const quantity = Number(quantityElement.value.trim());
     const foodElement = document.getElementById('enter_food');
@@ -28,41 +29,42 @@ function addItem(is_shopping_list) {
 
     const itemList = document.getElementById('itemlist');
 
-    const items = document.querySelectorAll('#itemlist li .item-name')
-    let ret = false;
-    let foods = []
+    const items = document.querySelectorAll('#itemlist li .item-name');
+    let found = false;
+    let foods = [];
     items.forEach((v, idx) => {
-        const quantity_elm = document.querySelector(`#${v.parentElement.id} .item-quantity`);
+        const quantityElm = document.querySelector(`#${v.parentElement.id} .item-quantity`);
         if (v.innerText == food) {
-            ret = true;
-            quantity_elm.innerText = Number(quantity_elm.innerText) + quantity
+            found = true;
+            quantityElm.innerText = Number(quantityElm.innerText) + quantity;
         }
         let obj = {
             name: v.innerText,
-            quantity: Number(quantity_elm.innerText),
+            quantity: Number(quantityElm.innerText),
+        };
+        if (!isShoppingList) {
+            obj.expiry = document.querySelectorAll('#itemlist li .expiry-date')[idx].value;
         }
-        if (!is_shopping_list) {
-            obj.expiry = document.querySelectorAll('#itemlist li .expiry-date')[idx].value
-        }
-        foods.push(obj)
-        console.log(foods)
-    })
-    if (!ret) {
+        foods.push(obj);
+    });
+
+    if (!found) {
         let obj = {
             name: food,
             quantity: quantity,
+        };
+        if (!isShoppingList) {
+            obj.expiry = null;
         }
-        if (!is_shopping_list) {
-            obj.expiry = null
-        }
-        foods.push(obj)
+        foods.push(obj);
     }
-    foods_info = foods
-    let save_to = 'pantry_items'
-    if (is_shopping_list) save_to = 'shopping_list'
-    localStorage.setItem(save_to, JSON.stringify(foods_info))
-    updateUser()
-    if (ret) return;
+
+    foodsInfo = foods;
+    let saveTo = 'pantry_items';
+    if (isShoppingList) saveTo = 'shopping_list';
+    localStorage.setItem(saveTo, JSON.stringify(foodsInfo));
+    updateUser();
+    if (found) return;
 
     const newItem = document.createElement('li');
     newItem.id = generateAlphabeticalID(10);  // Generate unique ID
@@ -73,30 +75,29 @@ function addItem(is_shopping_list) {
         <span class="item-name">${food}</span>
     `;
 
-    if (!is_shopping_list) {
+    if (!isShoppingList) {
         newItem.innerHTML += `
             <input type="date" id="expiry_date_${newItem.id}" class="expiry-date">
             <button class="remove-item" onclick="removeItem(this)">Remove</button>
             <button class="clear-expiry" onclick="clearExpiry(this)">Clear Expiry</button>
-        ` // dont delete this line
+        `;
         // Update expiry date span on item addition (optional)
         const expiryDateInput = newItem.querySelector('.expiry-date');
         expiryDateInput.addEventListener('change', () => {
             const date = document.getElementById(`expiry_date_${newItem.id}`).value;
-            for (let i = 0; i < foods_info.length; i++) {
-                if (foods_info[i]["name"] == food) {
-                    console.log(date)
-                    foods_info[i]["expiry"] = date
-                    break
+            for (let i = 0; i < foodsInfo.length; i++) {
+                if (foodsInfo[i]["name"] == food) {
+                    foodsInfo[i]["expiry"] = date;
+                    break;
                 }
             }
-            localStorage.setItem('pantry_items', JSON.stringify(foods_info))
-            updateUser()
+            localStorage.setItem('pantry_items', JSON.stringify(foodsInfo));
+            updateUser();
         });
     } else {
         newItem.querySelectorAll('.item-quantity, .item-name').forEach((e) => {
-            e.style.color = text_colors[color]
-        })
+            e.style.color = textColors[color];
+        });
     }
 
     itemList.appendChild(newItem);
@@ -104,64 +105,55 @@ function addItem(is_shopping_list) {
 
 function removeItem(button) {
     button.parentElement.remove();
-    let name = button.parentElement.querySelector('.item-name').innerText
-    console.log(name)
-    for (let i = 0; i < foods_info.length; i++) {
-        if (foods_info[i].name === name) {
-            foods_info.splice(i, 1);
-            break
+    let name = button.parentElement.querySelector('.item-name').innerText;
+    for (let i = 0; i < foodsInfo.length; i++) {
+        if (foodsInfo[i].name === name) {
+            foodsInfo.splice(i, 1);
+            break;
         }
     }
-    localStorage.setItem('pantry_items', JSON.stringify(foods_info))
-    updateUser()
+    localStorage.setItem('pantry_items', JSON.stringify(foodsInfo));
+    updateUser();
 }
 
 function clearExpiry(button) {
     const expiryInput = button.parentElement.querySelector('.expiry-date');
     expiryInput.value = "";
-    let name = button.parentElement.querySelector('.item-name').innerText
-    for (let i = 0; i < foods_info.length; i++) {
-        if (foods_info[i].name === name) {
-            foods_info[i].expiry = null
-            break
+    let name = button.parentElement.querySelector('.item-name').innerText;
+    for (let i = 0; i < foodsInfo.length; i++) {
+        if (foodsInfo[i].name === name) {
+            foodsInfo[i].expiry = null;
+            break;
         }
     }
-    localStorage.setItem('pantry_items', JSON.stringify(foods_info))
-    updateUser()
+    localStorage.setItem('pantry_items', JSON.stringify(foodsInfo));
+    updateUser();
 }
 
-/**
- * @param {HTMLInputElement} header
- */
 function headerKey(header) {
-    /**
-     * @param {KeyboardEvent} e 
-     * @returns 
-     */
-    function eventHandler(e) {
+    return function (e) {
         var keyCode = e.code || e.key;
         if (keyCode !== 'Enter') {
             return;
         }
-        let new_header = document.createElement('span')
-        new_header.classList.add('finished-header')
-        new_header.innerText = header.value
-        header.replaceWith(new_header)
-    }
-    return eventHandler
+        let newHeader = document.createElement('span');
+        newHeader.classList.add('finished-header');
+        newHeader.innerText = header.value;
+        header.replaceWith(newHeader);
+    };
 }
 
 function addHeader() {
-    let header = document.createElement('input')
-    header.type = 'text'
-    header.placeholder = 'type your header/title here!'
-    header.classList.add('list_header')
-    let list = document.getElementById('itemlist')
-    let br = document.createElement('br')
-    list.appendChild(header)
-    list.appendChild(br)
+    let header = document.createElement('input');
+    header.type = 'text';
+    header.placeholder = 'type your header/title here!';
+    header.classList.add('list_header');
+    let list = document.getElementById('itemlist');
+    let br = document.createElement('br');
+    list.appendChild(header);
+    list.appendChild(br);
 
-    header.addEventListener('keypress', headerKey(header))
+    header.addEventListener('keypress', headerKey(header));
 }
 
 document.getElementById('mode-toggle-checkbox').addEventListener('change', function() {
@@ -169,18 +161,18 @@ document.getElementById('mode-toggle-checkbox').addEventListener('change', funct
 });
 
 function login() {
-    let email = document.getElementById('email').value
-    let password = document.getElementById('password').value
-    let token = localStorage.getItem('token')
-    console.log('login', email, password, token)
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    let token = localStorage.getItem('token');
+    console.log('login', email, password, token);
 
     if (token) {
         fetch(`http://localhost:3000/api/users/${token}/`)
         .then((response) => (response.json()))
         .then((response) => {
-            localStorage.setItem('pantry_items',JSON.stringify(response.pantry_items))
-            localStorage.setItem('shopping_list',JSON.stringify(response.shopping_list))
-        })
+            localStorage.setItem('pantry_items',JSON.stringify(response.pantry_items));
+            localStorage.setItem('shopping_list',JSON.stringify(response.shopping_list));
+        });
     } else {
         fetch(`http://localhost:3000/api/users/getByEmail/${email}`, {
             method: "POST",
@@ -191,29 +183,22 @@ function login() {
         })
         .then(response => (response.json()))
         .then((response) => {
-            localStorage.setItem('pantry_items',JSON.stringify(response.pantry_items))
-            localStorage.setItem('shopping_list',JSON.stringify(response.shopping_list))
-            localStorage.setItem('token',JSON.stringify(response.token))
-        })
+            localStorage.setItem('pantry_items',JSON.stringify(response.pantry_items));
+            localStorage.setItem('shopping_list',JSON.stringify(response.shopping_list));
+            localStorage.setItem('token',JSON.stringify(response.token));
+        });
     }
 }
 
 function logout() {
-    localStorage.setItem('token', '')
-}
-
-function storageToList (){
-    pantry_items = localStorage.getItem('pantry_items')
-    shopping_list = localStorage.getItem('shopping_list')
-
-
+    localStorage.setItem('token', '');
 }
 
 function signup() {
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
-    var items = JSON.parse(localStorage.getItem('pantry_items'))
-    var shopping_list = JSON.parse(localStorage.getItem('shopping_list'))
+    var items = JSON.parse(localStorage.getItem('pantry_items'));
+    var shoppingList = JSON.parse(localStorage.getItem('shopping_list'));
 
     fetch(`http://localhost:3000/api/users/`, {
         method: "POST",
@@ -224,19 +209,19 @@ function signup() {
             email: email,
             password: password,
             pantry_items: items || [],
-            shopping_list: shopping_list || [] //Get a list with objects
+            shopping_list: shoppingList || [] //Get a list with objects
         })
     })
     .then((response) => response.text())
     .then((response) => {
-        localStorage.setItem('token', response)
-    })
+        localStorage.setItem('token', response);
+    });
 }
 
 function updateUser() {
-    var token = localStorage.getItem('token')
-    var items = JSON.parse(localStorage.getItem('pantry_items'))
-    var shopping_list = JSON.parse(localStorage.getItem('shopping_list'))
+    var token = localStorage.getItem('token');
+    var items = JSON.parse(localStorage.getItem('pantry_items'));
+    var shoppingList = JSON.parse(localStorage.getItem('shopping_list'));
 
     fetch(`http://localhost:3000/api/users/${token}`, {
         method: "PUT",
@@ -245,37 +230,34 @@ function updateUser() {
         },
         body: JSON.stringify({
             pantry_items: items,
-            shopping_list: shopping_list
+            shopping_list: shoppingList
         })
     })
     .then((response) => (response.text()))
     .then((response) => {
-        console.log(response)
-    })
+        console.log(response);
+    });
 }
 
 function chat() {
-    var chat_prompt = document.getElementById('enter_prompt').value
-    console.log(chat_prompt)
+    var chatPrompt = document.getElementById('enter_prompt').value;
+    console.log(chatPrompt);
     fetch(`http://localhost:3000/api/chat/`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            prompt: chat_prompt
+            prompt: chatPrompt
         })
     })
     .then((response) => (response.text()))
-    .then((text) => document.getElementById('Chatbot-Output').innerText+='\n____________________\n'+text)
+    .then((text) => document.getElementById('Chatbot-Output').innerText+='\n____________________\n'+text);
 }
 
-/**
- * @param {HTMLLIElement} color_elm 
- */
-function color_change(color_elm) {
-    let colors = document.querySelectorAll('.color-item')
-    colors.forEach((c) => {c.classList.remove('selected-color')})
-    color_elm.classList.add('selected-color')
-    color = color_elm.id
+function colorChange(colorElm) {
+    let colors = document.querySelectorAll('.color-item');
+    colors.forEach((c) => {c.classList.remove('selected-color');});
+    colorElm.classList.add('selected-color');
+    color = colorElm.id;
 }
